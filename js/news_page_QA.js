e803c493d0f4dfbbaf87e9550500a130fa3b9858
Chan Yoon <ch4n.yn@chans-mbp.attlocal.net>
2018-05-13 18:18:53 -0700

    first commit

---
+++ js/news_page_QA.js
@@ -0,0 +1,110 @@
+function showQA1(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_one").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_one").css('display', 'none');
+        };
+}
+function showQA2(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_two").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_two").css('display', 'none');
+        };
+}
+function showQA3(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_three").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_three").css('display', 'none');
+        };
+}
+function showQA4(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_four").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_four").css('display', 'none');
+        };
+}
+function showQA5(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_five").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_five").css('display', 'none');
+        };
+}
+function showQA6(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_six").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_six").css('display', 'none');
+        };
+}
+function showQA7(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_seven").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_seven").css('display', 'none');
+        };
+}
+function showQA8(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_eight").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_eight").css('display', 'none');
+        };
+}
+function showQA9(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_nine").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_nine").css('display', 'none');
+        };
+}
+function showQA10(id) {
+
+    var question_num = document.getElementById(id);
+        if ($(question_num).css('display') === 'none') {
+            $(question_num).css('display', 'block');
+            $(".video_ten").css('display', 'block')
+        } else {
+            $(question_num).css('display', 'none');
+            $(".video_ten").css('display', 'none');
+        };
+}
---
+++ js/tools_page_USD.js
@@ -0,0 +1,240 @@
+function convertToUSD() { // FUNCTION FOR CONVERTING FROM USD TO COINS
+    "use strict";
+    
+    var selected = document.getElementById("dropmenu1");
+    var choice = selected.options[selected.selectedIndex].text; // option choice ex: btc,golem, ltc, etc
+    
+    var amount = document.getElementById("amount1");
+    
+    $("#amountCRYPT1").html(choice); // changes name to according opt chioce
+    
+    //uses xmlhttp request to grab BITCOIN api from bitcoinaverage.com
+    // and displays live price for BTC ONLY
+    var xbtc = new XMLHttpRequest();
+    var price;
+    
+    //conditionals for the choice word
+    //  ADD IN APIS FOR THE COINS
+
+    if (choice == "Bitcoin") { //BTC CONDITIONAL WORKS
+        
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD', true);
+        xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " BTC");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+        
+    } else if (choice == "Ethereum") { //ETH CONDITONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " ETH");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+            
+    } else if (choice == "Golem") { //GNT CONDITIONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=GNT&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " GNT");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+    } else if (choice == "Litecoin") { //LTC CONDITONAL WORKS
+       xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " LTC");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send(); 
+    } else if (choice == "Dash") { //DASH CONDITONAL WORKS
+       xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=DASH&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " DASH");
+            $("#testing1").html("Live Price: $" + price );
+    
+        }
+    };
+    xbtc.send();  
+        
+    } else if (choice =="Augur") { // AUGUR CONDITONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=REP&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " REP");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();  
+        
+    } else if (choice == "Dogecoin") {
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD1").html("You get: " + amount.value / price + " DOGE");
+            $("#testing1").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+        
+    } 
+}
+
+function convertFromUSD() { // FUNCTION FOR CONVERTING FROM COINS TO USD
+    "use strict";
+    
+    var selected = document.getElementById("dropmenu2");
+    var choice = selected.options[selected.selectedIndex].text; // option choice ex: btc,golem, ltc, etc
+    
+    var amount = document.getElementById("amount2");
+    
+    $("#amountCRYPT2").html(choice); // changes name to according opt chioce
+    
+    //uses xmlhttp request to grab BITCOIN api from bitcoinaverage.com
+    // and displays live price for BTC ONLY
+    var xbtc = new XMLHttpRequest();
+    var price;
+    
+    //conditionals for the choice word
+    //  ADD IN APIS FOR THE COINS
+
+    if (choice == "Bitcoin") { //BTC CONDITIONAL WORKS
+        
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD', true);
+        xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+        
+    } else if (choice == "Ethereum") { //ETH CONDITONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+            
+    } else if (choice == "Golem") { //GNT CONDITIONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=GNT&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+    } else if (choice == "Litecoin") { //LTC CONDITONAL WORKS
+       xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send(); 
+    } else if (choice == "Dash") { //DASH CONDITONAL WORKS
+       xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=DASH&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+    
+        }
+    };
+    xbtc.send();  
+        
+    } else if (choice =="Augur") { // AUGUR CONDITONAL WORKS
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=REP&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();  
+        
+    } else if (choice == "Dogecoin") {
+        xbtc.open('GET', 'https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=USD', true);
+    xbtc.onreadystatechange = function(){
+        if(xbtc.readyState == 4){
+            var ticker = JSON.parse(xbtc.responseText);
+            price = ticker.USD; // live price of bitcoin 
+            //converts  from the amount USD to BTC amount.
+            $("#amountUSD2").html("You get: " + amount.value * price + " USD");
+            $("#testing2").html("Live Price: $" + price );
+         
+        }
+    };
+    xbtc.send();
+        
+    }
+    
+}
+
+
